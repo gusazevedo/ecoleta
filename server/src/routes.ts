e6f245f64a235502d2ea2c020ad026a8fe35b7e6
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2020-06-02 18:32:14 -0300

    implementando insert e select com knex

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,59 @@
-import express from 'express';
+import express, { request } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
+routes.get('/itens', async (request, response) => { 
+    const itens = await knex('itens').select('*');
+
+    const serializedItens = itens.map(item => {
+        return { 
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+    
+    return response.json(serializedItens);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+        latitude,
+        longitude,
+        itens
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+        latitude,
+        longitude,
+        image: 'image-fake'
+    });
+
+    const point_id = insertedIds[0];
+
+    const poinsItens = itens.map((item_id: Number) => {
+        return {
+            item_id,
+            point_id,
+        };
+    });
+
+    await trx('point_itens').insert(poinsItens);
+
+    return response.json({ success: true });
 });
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express, { response, request } from 'express';
+import path from 'path';
+
 import routes from './routes';
 
 const app = express ();
@@ -6,7 +8,8 @@ const app = express ();
 // por padrão o express não entende qual o tipo da aplicação
 // então preciso dizer pra ele que eu preciso que ele entenda requisições tipo JSON
 app.use(express.json());
-
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
