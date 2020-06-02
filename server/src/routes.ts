fdaeb179a164262c9aaab547e97601740b64fcc5
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2020-06-02 20:00:15 -0300

    finalizando integração com banco de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,18 @@
 import express, { request } from 'express';
-import knex from './database/connection';
+
+import PointsConttroler from './controllers/PointsController';
+import ItensConttroler from './controllers/ItensController';
+
 
 const routes = express.Router();
+const pointsConttroler = new PointsConttroler();
+const itensControler = new ItensConttroler();
 
-routes.get('/itens', async (request, response) => { 
-    const itens = await knex('itens').select('*');
+routes.get('/itens', itensControler.index);
 
-    const serializedItens = itens.map(item => {
-        return { 
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-    
-    return response.json(serializedItens);
-});
+routes.post('/points', pointsConttroler.create);
+routes.get('/points', pointsConttroler.index);
+routes.get('/points/:id', pointsConttroler.show);
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-        latitude,
-        longitude,
-        itens
-    } = request.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-        latitude,
-        longitude,
-        image: 'image-fake'
-    });
-
-    const point_id = insertedIds[0];
-
-    const poinsItens = itens.map((item_id: Number) => {
-        return {
-            item_id,
-            point_id,
-        };
-    });
-
-    await trx('point_itens').insert(poinsItens);
-
-    return response.json({ success: true });
-});
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 import routes from './routes';
 
-const app = express ();
+const app = express();
 
 // por padrão o express não entende qual o tipo da aplicação
 // então preciso dizer pra ele que eu preciso que ele entenda requisições tipo JSON
